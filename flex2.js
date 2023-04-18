a278f74fe32b05eca7b6c6c95ad2d6a47a6c4b6d
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-04-18 20:35:17 +0200

    F

--- flex2.js
+++ flex2.js
@@ -3,27 +3,19 @@
 
 const filas = 3;
 const columnas = 5;
-const espacio = 20;
-
-
-let cosa = ' ';
+let output = ' ';
 
 
 for (let i = 0; i < filas; i++) {
-  for (let j = 0; j < espacio; j++) {
-    cosa += '&nbsp;';
-  }
   for (let j = 0; j < columnas; j++) {
-
-
     const orden =(i + j)>=(filas - 1)&&(j - i)<=(filas - 1);
     if (orden) {
-      cosa += 'o&nbsp;';
+        output += 'o&nbsp;';
     } else {
-      cosa += '&nbsp;&nbsp;';
+        output += '&nbsp;&nbsp;';
     }
   }
-  cosa += '<br>';
+  output += '<br>';
 }
 // document.write(cosa);
 
@@ -34,6 +26,7 @@ var cuadro = prompt("Diga en qué cuadro quiere saludar (1 a 5)","");
 cuadro = parseInt(cuadro);
 // cuadro será una variable numérica, supuestamente entre 1 y 5
 
+var colorSeleccionado = prompt("Elija un color para el texto (red, green, blue)", "");
 
 var estecuadro;
 
@@ -41,27 +34,50 @@ var estecuadro;
 switch (cuadro) {
     case 1:
         estecuadro =  document.getElementById("c1");
-        estecuadro.innerHTML=cosa;
+        estecuadro.innerHTML=output;
         break;
     case 2:
         estecuadro =  document.getElementById("c2");
-        estecuadro.innerHTML=cosa;
+        estecuadro.innerHTML=output;
         break;
     case 3:
         estecuadro =  document.getElementById("c3");
-        estecuadro.innerHTML=cosa;
+        estecuadro.innerHTML=output;
         break;
     case 4:
         estecuadro =  document.getElementById("c4");
-        estecuadro.innerHTML=cosa;
+        estecuadro.innerHTML=output;
         break;
     case 5:
         estecuadro =  document.getElementById("c5");
-        estecuadro.innerHTML=cosa;
+        estecuadro.innerHTML=output;
         break;
    
     default:
         alert ("Numero no valido");
         exit(-1);
 }
+estecuadro.style.color = colorSeleccionado;
+
+//LA FORMA DE ABAJO TE LO PIDE DENTRO DEL CUADRO UNA VEZ ELEGIDO YA EL LUGAR
+/* // Pedir al usuario que seleccione un color utilizando un elemento select
+var colorSelect = document.createElement("select");
+colorSelect.innerHTML = `
+  <option value="red">Rojo</option>
+  <option value="green">Verde</option>
+  <option value="blue">Azul</option>
+`;
+
+// Agregar el elemento select al cuadro seleccionado
+estecuadro.appendChild(colorSelect);
+
+// Establecer el color del texto del cuadro seleccionado según el color seleccionado por el usuario
+colorSelect.addEventListener("change", function() {
+  var color = colorSelect.value;
+  estecuadro.style.color = color;
+}); */
+
+
+
+
 
