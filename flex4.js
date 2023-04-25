81fa80a65973ca76c0db43e1c65a097ae0fbb953
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-04-25 18:49:12 +0200

    s

--- flex4.js
+++ flex4.js
@@ -1,61 +1,55 @@
 'use strict';
+// nos ayuda a no dejar pasar nungún error
+// Programa que solamente define funciones que son invocadas e inicializadas de forma global
+
+
+
+
+//defino variables globales
 var color = 'white';
 
 //SOLO arrays, esta es una forma aunq tambiens e pueden meter directamente dentro de los [] separado pro comas 
-//SE CAMBIA EL BACK Y EL COLOR DE LA LETRA SEGUNE STE ASOCIADO EN EL ARRAY 
-var colorFn=[];
-colorFn[0]='magenta';
-colorFn[1]='blue';
-colorFn[2]='red';
-colorFn[3]='black';
-colorFn[4]='yellow';
+//SE CAMBIA EL BACK Y EL COLOR DE LA LETRA SEGUNE STE ASOCIADO EN EL ARRAY
+// trabajamos con arrays
 
-var colorFx= new Array ["blue","white","red","yellow","black"]; //declarar un nuevo array
+var colorFr= new Array ['dodgerblue','rgb(30, 255, 206)','rgb(255, 30, 255)','rgb(255, 165, 30)','rgb(158, 255, 30)']
+var colorFn= [];
+colorFn[0]= 'magenta';
+colorFn[1]= 'blue';
+colorFn[2]= 'black';
+colorFn[3]= 'yellow';
+colorFn[4]= 'red';
 
-function pinturaInicial(){
-for(let i=0; i<5; i++){
-  let identificador= "c" +(i+1);
-  let elemento=document.getElementById(identificador);
-  elemento.style.backgroundColor=colorFn[i];
-  elemento.style.color=colorFx[i];
+
+function pinturaInicial () {
+    for (let i=0; i< 5 ; i++){
+    let identificador="c"+(i+1);
+    let elemento=document.getElementById(identificador);
+    elemento.style.barkgroundColor=colorFn[i];
+    elemento.style.color=colorFr[i];
+    }
 }
-}
+pinturaInicial(); //si quiero llamar una funcion cambio el nombre de esto
 //ahora al color lo cogemos con una funcion.
 //este programa solo define funciones invocadas e inicializadas de forma global con htlm
-function obtieneColor(identificador) {
-  let id = "c" + identificador; //modo string lo concatena
-  let cuadro = document.getElementById(id);
-  color=getComputedStyle(cuadro).backgroundColor;
-  console.log(color);
- 
+function obtenerColor(identificador) {
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);
+    color = getComputedStyle(cuadro).backgroundColor;  // para sacar los colores que se le asignó al cuadro en el css
+    console.log(color);
 }
+
+
+// para poner color a la palabra de dentro del cuadrado
 function saludar(identificador) {
-  let id = "c" + identificador; //modo string lo concatena
-  let cuadro = document.getElementById(id);
-  cuadro.style.color = color;
-  cuadro.innerHTML = "HOLA";
-
+    let id = "c" + identificador;
+    let cuadro = document.getElementById(id);
+    cuadro.style.color = color;
+    cuadro.innerHTML = "HOLA";
 }
 
-pinturaInicial();
 
-//LA FORMA DE ABAJO TE LO PIDE DENTRO DEL CUADRO UNA VEZ ELEGIDO YA EL LUGAR
-/* // Pedir al usuario que seleccione un color utilizando un elemento select
-var colorSelect = document.createElement("select");
-colorSelect.innerHTML = `
-  <option value="red">Rojo</option>
-  <option value="green">Verde</option>
-  <option value="blue">Azul</option>
-`;
 
-// Agregar el elemento select al cuadro seleccionado
-estecuadro.appendChild(colorSelect);
-
-// Establecer el color del texto del cuadro seleccionado según el color seleccionado por el usuario
-colorSelect.addEventListener("change", function() {
-  var color = colorSelect.value;
-  estecuadro.style.color = color;
-}); */
 
 
 
